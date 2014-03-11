71c12970d7966dcc604e129325efef3a7003b845
shinobicorp <danielhudson@shinobicorp.com>
2014-03-11 12:52:32 +0000

    Updating for Ubuntu/Vagrant.

--- nginx-vhost/nginx-vhost.sh
+++ nginx-vhost/nginx-vhost.sh
@@ -4,10 +4,25 @@ VERSION=v0.1
 
 BASEDIR=$(dirname $0)
 DOMAIN=$1
+
+# Full path to vhost available - you should create this if it doesn't exist.
 VHOST_AVAILABLE=/etc/nginx/sites-available
+
+# Should script chwon newly created vhosts?
+VHOST_CHOWN=true
+
+# Which User/Group do we chown the vhost with?
+VHOST_CHOWN_USER=www-data
+VHOST_CHOWN_GROUP=www-data
+
+# Full path to vhost enabled - this should exist already as part of your nginx installation
 VHOST_ENABLED=/etc/nginx/sites-enabled
+
+# Place templatae in same directory as this script.
 VHOST_TEMPLATE=$BASEDIR/nginx-vhost-template.txt
-WWW_ROOT=/mnt/www
+
+# Vhost Directory Root.
+WWW_ROOT=/var/www
 
 MSG_ERR=ERR:
 MSG_INFO=INFO:
@@ -21,78 +36,90 @@ echo "-> nginx-vhost $VERSION"
 if [[ -z "$DOMAIN" ]]; then
 	echo "-> $MSG_ERR Please pass domain name, eg ./nginx-vhost example.com"
 	echo "-> $MSG_DONE"
-    echo ""
-    exit 1;
+	echo ""
+	exit 1;
 fi
 
 # Check for sudo
 if [[ "$(whoami)" != "root" ]]; then
 
-    echo "-> $MSG_ERR Sorry, you are not root - sudo?"
-    echo "-> $MSG_DONE"
-    echo ""
-    exit 1
-fi
-
-# Check for VHOST_DIR
-if [[ ! -d "$VHOST_AVAILABLE" ]]; then
-    echo "-> $MSG_ERR $VHOST_AVAILABLE doesn't exist"
-    echo "-> $MSG_DONE"
-    echo ""
+	echo "-> $MSG_ERR Sorry, you are not root - try sudo?"
+	echo "-> $MSG_DONE"
+	echo ""
 	exit 1
 fi
 
 # Check for VHOST_DIR
+if [[ ! -d "$VHOST_AVAILABLE" ]]; then
+	read -p "-> MSG_ERR $VHOST_AVAILABLE doesn't exist. Create it?" -n 1 -r
+	echo ""
+
+	if [[ $REPLY =~ ^[Yy]$ ]]
+	then
+		mkdir -p $VHOST_AVAILABLE
+	else
+		echo "-> $MSG_DONE"
+		echo ""
+		exit 1
+	fi
+fi
+
+# Check for VHOST_DIR
 if [[ ! -d "$VHOST_ENABLED" ]]; then
-    echo "-> $MSG_ERR $VHOST_ENABLED doesn't exist"
-    echo "-> $MSG_DONE"
-    echo ""
-    exit 1
+	read -p "-> MSG_ERR $VHOST_ENABLED doesn't exist. Create it?" -n 1 -r
+	echo ""
+
+	if [[ $REPLY =~ ^[Yy]$ ]]
+	then
+		mkdir -p $VHOST_ENABLED
+	else
+		echo "-> $MSG_DONE"
+		echo ""
+		exit 1
+	fi
 fi
 
 # Check for WWW_ROOT
 if [[ ! -d "$WWW_ROOT" ]]; then
-    echo "-> $MSG_ERR $WWW_ROOT doesn't exist"
-    echo "-> $MSG_DONE"
-    echo ""
+	echo "-> $MSG_ERR $WWW_ROOT doesn't exist"
+	echo "-> $MSG_DONE"
+	echo ""
 	exit 1
 fi
 
 # Check for WWW_ROOT/DOMAIN
 if [[ -d "$WWW_ROOT/$DOMAIN" ]]; then
-    echo "-> $MSG_ERR $WWW_ROOT/$DOMAIN already exists"
-    echo "-> $MSG_DONE"
-    echo ""
-    exit 1
+	echo "-> $MSG_ERR $WWW_ROOT/$DOMAIN already exists, please (re)move it first"
+	echo "-> $MSG_DONE"
+	echo ""
+	exit 1
 fi
 
 # Check for VHOST_DIR/DOMAIN
 if [[ -f "$VHOST_AVAILABLE/$DOMAIN" ]]; then
-	echo "-> $MSG_ERR $VHOST_AVAILABLE/$DOMAIN already exists"
-    echo "-> $MSG_DONE"
-    echo ""
-    exit 1
+	echo "-> $MSG_ERR $VHOST_AVAILABLE/$DOMAIN already exists, please (re)move it first"
+	echo "-> $MSG_DONE"
+	echo ""
+	exit 1
 fi
 
 # Check for VHOST_TEMPLATE
 if [[ ! -f "$VHOST_TEMPLATE" ]]; then
-    echo "-> $MSG_ERR $VHOST_TEMPLATE doesn't exist"
-    echo "-> $MSG_DONE"
-    echo ""
-    exit 1
+	echo "-> $MSG_ERR $VHOST_TEMPLATE doesn't exist"
+	echo "-> $MSG_DONE"
+	echo ""
+	exit 1
 fi
 
-# Create vhost directories
+# Create Vhost directories
 mkdir -p $WWW_ROOT/$DOMAIN
-
 mkdir $WWW_ROOT/$DOMAIN/site
 mkdir $WWW_ROOT/$DOMAIN/site/web
-
 mkdir $WWW_ROOT/$DOMAIN/ssl
 mkdir $WWW_ROOT/$DOMAIN/log
 
 echo "Welcome to $DOMAIN" > $WWW_ROOT/$DOMAIN/site/web/index.html
-chown -R www-data:www-data $WWW_ROOT/$DOMAIN
+if [ "$VHOST_CHOWN" = true] chown -R $VHOST_CHOWN_USER:$VHOST_CHOWN_GROUP $WWW_ROOT/$DOMAIN
 
 # Create vhost record from template
 sed -e "s;%WWW_ROOT%;$WWW_ROOT;" -e "s;%DOMAIN%;$DOMAIN;" $VHOST_TEMPLATE > $VHOST_AVAILABLE/$DOMAIN
